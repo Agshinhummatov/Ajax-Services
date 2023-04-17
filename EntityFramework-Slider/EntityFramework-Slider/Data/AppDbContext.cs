fa60fd415d3bbcc33ec9fb6e9eb617a84d37a0a7
Agshinhummatov <agshinshh@code.edu.az>
2023-04-18 01:36:14 +0400

    Homework

---
+++ EntityFramework-Slider/EntityFramework-Slider/Data/AppDbContext.cs
@@ -0,0 +1,57 @@
+﻿using EntityFramework_Slider.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EntityFramework_Slider.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Slider> Sliders { get; set; }
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<SliderInfo> SliderInfos { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductImage> ProductImages { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Setting> Setting { get; set; }
+
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasQueryFilter(m => !m.SoftDelete); //bir defe yaziriq istifade edirik soft delete edirmik
+
+
+
+            modelBuilder.Entity<Setting>()  // seeting key value tipinden bizden datalar isdeyir burdan yaziriq dusur table
+           .HasData(
+            new Setting
+            {
+                Id = 1,
+                Key = "HeaderLogo",
+                Value = "logo.png"
+            },
+            new Setting
+            {
+                Id = 2,
+                Key = "Phone",
+                Value = "35724544389953"
+            },
+            new Setting
+            {
+                Id = 3,
+                Key = "Email",
+                Value = "aqsin@code.edu.az"
+            }
+           );
+        }
+
+
+
+
+
+
+
+    }
+}
